aeec72526506399d51afc68533e00668213b8d30
aashishpoudel <poulaashish@yahoo.com>
2017-11-25 12:52:49 -0800

    More webpages added, layout edited

--- script1.py
+++ script1.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
-def home():
-    return "My website goes here. Homepage"
+def root():
+    return render_template("home.html")
 
 @app.route('/about/')
 def about():
-    return "This is my About Page"
+    return render_template("about.html")
+
+@app.route('/hidden/')
+def hidden():
+    return "Website in Heroku -- By Aashish Poudel"
 
 @app.route('/page1/')
 def page1():
@@ -19,7 +23,7 @@ def page2():
     return render_template("page2.html")
 
 @app.route('/home/')
-def navigation():
+def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
